7cc0034b2497d471a72036f99128b29a9af1f8c7
Nahda Fauziyah Zahra <nahda15@mhs.if.its.ac.id>
2018-01-15 09:32:26 +0700

    Auto stash before merge of "master" and "origin/master"
    fix tele

--- index.php
+++ index.php
@@ -105,10 +105,11 @@ ddsmoothmenu.init({
 })
 
 </script> 
-<script id="chatBroEmbedCode">/* Chatbro Widget Embed Code Start */
+<script id="chatBroEmbedCode">
+  /* Chatbro Widget Embed Code Start */
   function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','https://www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
-  /* Chatbro Widget Embed Code End */
-  ChatbroLoader({encodedChatId: '7gYQ',containerDivId: 'chatbroHistory',useStandardHistoryWidgetSettings: true});
+/* Chatbro Widget Embed Code End */
+ChatbroLoader({encodedChatId: '7gYQ'});
 </script>
 </head>
 
@@ -134,7 +135,8 @@ ddsmoothmenu.init({
     </div>
     <!-- Frame Works 2-->
     <div id="templatemo_middle_r">
-      
+      <div id="chatbroHistory">
+      </div>
     </div>
 </div> <!-- END of slider -->
 
