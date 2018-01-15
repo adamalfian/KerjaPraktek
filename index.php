015e778fe6a3114638a1080956c9d143733e92e3
Nahda Fauziyah Zahra <nahda15@mhs.if.its.ac.id>
2018-01-15 08:22:27 +0700

    tambah telegram

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Work Center Theme - Free CSS Template</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript" src="js/swfobject/swfobject.js"></script>
+<link rel="stylesheet" type="text/css" href="css/slick.css">
+<link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+  <style type="text/css">
+    html, body {
+      margin: 0;
+      padding: 0;
+    }
+
+    * 
+    {
+      box-sizing: border-box;
+    }
+
+    .slider {
+        width: 100%;
+        margin: 100px auto;
+    }
+
+    .slick-slide {
+      margin: 0px 20px;
+    }
+
+    .slick-slide img {
+      width: 100%;
+    }
+
+    .slick-prev:before,
+    .slick-next:before {
+      color: black;
+    }
+
+
+    .slick-slide {
+      transition: all ease-in-out .3s;
+      opacity: .2;
+    }
+    
+    .slick-active {
+      opacity: .5;
+    }
+
+    .slick-current {
+      opacity: 1;
+    }
+  </style>
+        
+<script type="text/javascript">
+  var flashvars = {};
+  flashvars.cssSource = "css/piecemaker.css";
+  flashvars.xmlSource = "piecemaker.xml";
+	
+  var params = {};
+  params.play = "true";
+  params.menu = "false";
+  params.scale = "showall";
+  params.wmode = "transparent";
+  params.allowfullscreen = "true";
+  params.allowscriptaccess = "always";
+  params.allownetworking = "all";
+  
+  swfobject.embedSWF('piecemaker.swf', 'piecemaker', '960', '440', '10', null, flashvars,    
+  params, null);
+
+</script>
+
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+	mainmenuid: "templatemo_menu", //menu DIV id
+	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+	classname: 'ddsmoothmenu', //class added to menu's outer DIV
+	//customtheme: ["#1c5a80", "#18374a"],
+	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script> 
+<script id="chatBroEmbedCode">/* Chatbro Widget Embed Code Start */
+  function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','https://www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
+  /* Chatbro Widget Embed Code End */
+  ChatbroLoader({encodedChatId: '7gYQ',containerDivId: 'chatbroHistory',useStandardHistoryWidgetSettings: true});
+</script>
+</head>
+
+<body>
+
+<div id="templatemo_header_wrapper">
+    <div id="templatemo_header">
+        <div id="site_title"><a>SOA<span style="color: red"> Portal</span></a></div>
+        <div id="templatemo_menu" class="ddsmoothmenu">
+            <ul>
+                <li><a href="https://docs.google.com/spreadsheets/d/16zmsnK-yCQSDDR4l2Wi0Q4JDvJsmr6ljtJACRPFTZ4U/edit#gid=0">Daftar Requirement</a></li>
+                <li><a href="https://docs.google.com/spreadsheets/d/110zSWm-pK67-EUFdE7MTeyw9Hg0YJyfccFgoVALUwqk/edit#gid=0">Daftar Dev-to-Prod</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of templatemo_menu -->
+    </div> <!-- END of header -->
+</div>
+
+<div id="templatemo_middle_wrapper">
+    <!-- Frame Works 1-->
+    <div id="templatemo_middle_l" >
+        <center><img src="images/SOA_MAP.jpg" width="80%" height="60%"></center>
+    </div>
+    <!-- Frame Works 2-->
+    <div id="templatemo_middle_r">
+      
+    </div>
+</div> <!-- END of slider -->
+
+<div id="templatemo_main_top"></div>
+<div id="templatemo_main">
+    <div class="col_12 float_l">
+        <h2>Operasional Support</h2>
+    </div>
+    <div class="cleaner h20"></div>
+    <div class="fp_box5">
+        <a href="http://soaportal.telkom.co.id/tenossBBDev/index.php">
+            <img src="images/tennos_BB_development.jpg" alt="Image 01" />
+        </a>
+    </div>
+    <div class="fp_box5">
+        <a href="http://soaportal.telkom.co.id/infusionDev/index.php">
+            <img src="images/infusion_development.jpg" alt="Image 05" />
+        </a>
+    </div>
+    <div class="fp_box5">
+        <a href="http://soaportal.telkom.co.id/flexiMon/index.php">
+            <img src="images/flexi_production.jpg" alt="Image 03" />
+        </a>
+    </div>
+    <div class="fp_box5 no_margin_right">
+        <a href="http://soaportal.telkom.co.id/eaiops/index.php">
+            <img src="images/EAIO.jpg" alt="Image 04" />
+        </a>
+    </div>
+
+    <div class="cleaner h50"></div>
+     <div class="fp_box5">
+        <a href="http://mws.telkom.co.id:8585/">
+            <img src="images/tennos_BB_production.jpg" alt="Image 02" />
+        </a>
+    </div>
+    <div class="fp_box5">
+        <a href="http://soaportal.telkom.co.id/infusionProd/index.php">
+            <img src="images/infusion_production.jpg" alt="Image 06" />
+        </a>
+    </div>
+    <div class="fp_box5">
+        <a href="http://soaportal.telkom.co.id/sgwMon/index.php">
+            <img src="images/EAI.jpg" alt="Image 07" />
+        </a>
+    </div>
+    <div class="fp_box5 no_margin_right">
+        <a href="http://soaportal.telkom.co.id/sap-crm/index.php">
+            <img src="images/SAP_CRM.jpg" alt="Image 08" />
+        </a>
+    </div>
+
+    <div class="cleaner h20"></div>
+
+    <div class="cleaner"></div>
+ <section class="regular slider">
+    <div>
+        <a href="https://www.google.co.id/">
+          <img src="http://placehold.it/350x300?text=1">
+        </a>
+    </div>
+    <div>
+      <img src="http://placehold.it/350x300?text=2">
+    </div>
+    <div>
+      <img src="http://placehold.it/350x300?text=3">
+    </div>
+    <div>
+      <img src="http://placehold.it/350x300?text=4">
+    </div>
+    <div>
+      <img src="http://placehold.it/350x300?text=5">
+    </div>
+    <div>
+      <img src="http://placehold.it/350x300?text=6">
+    </div>
+  </section>
+</div> <!-- END of main -->
+
+
+<div id="templatemo_footer_wrapper">
+	<div id="templatemo_footer">
+    <iframe style="width: 100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vReZX-4J6DS9EETOR1gHUXnkD7CGVvCVCnPbPUOTY1KU_pJi6STf52dVUYCGLYz0FdZleqo1thpqZ9k/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe> 
+     <div class="cleaner"></div> 		 
+      <iframe style="width: 100%" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRPp1rCShOrWfTtrYIXwxnpUrKIVW2dUqOD97k-SHA9fLfpcUywwNCM3EaMd_Hf5mv9S19PKO5LOm_R/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>       
+  </div>
+   
+
+   
+
+</div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+    });
+</script>
+
+</body>
+</html>
